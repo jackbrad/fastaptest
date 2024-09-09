25cbf6f8835c8716693ec3e135b6b32e55d478ec
jackbrad <jackb@live.com>
2024-09-08 22:12:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+
+import boto3
+from fastapi import FastAPI, HTTPException
 import subprocess
+import os
 
 app = FastAPI()
-
+s3 = boto3.client('s3')
 
 @app.get("/")
 def root():
@@ -14,3 +17,6 @@ async def generate_diagram():
     cmd = "docker run --rm -u 0:0 -v /home/ec2-user/mmd_examples/data:/data minlag/mermaid-cli -i diagram.mmd -o output-diagram.png"
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return {"output": result.stdout, "error": result.stderr}
+
+
+
